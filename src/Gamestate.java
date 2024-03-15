6a252afb03d6e1493908005cd4ca1c5536cd5e73
xalfer <xaf.atmes@gmail.com>
2024-03-15 12:56:51 +0100

    add api for gamestate

---
+++ src/Gamestate.java
@@ -0,0 +1,10 @@
+import java.lang.IllegalArgumentException;
+
+public class Gamestate {
+    Gamestate() {
+    
+    }
+        
+    public void put(int x, int y, Player player) throws IllegalArgumentException {}
+    public Player hasWon() { return null; }
+}
